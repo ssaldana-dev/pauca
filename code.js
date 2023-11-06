63644b315fd5a0fc9ca2b8d0167f63ad27f7c7a1
Sergio Saldaña <ssaldana.dev@gmail.com>
2023-11-05 20:02:20 -0600

    Ended the responsive page

--- code.js
+++ code.js
@@ -3,34 +3,55 @@ const responsiveMenu = document.querySelector('.responsive-menu');
 const responsiveBar = document.querySelector('.responsive-menu_bars');
 const responsiveNav = document.querySelector('#navigation-menu');
 const menuAnchors = document.querySelectorAll('.navigation-menu_anchor');
+const successStoriesGallery = document.querySelector('#success-stories-gallery');
+const successStoriesButton = document.querySelector('#see-more');
 responsiveMenu.addEventListener('click', deployMenu);
+successStoriesButton.addEventListener('click', deploySuccessStories);
 
 for (anchor of menuAnchors) {
     anchor.addEventListener('click', withdrawMenu);
 }
 
-counter = 0;
+let isMenuActive = false;
 
 function deployMenu () {
-    if (counter == 0) {
+    if (isMenuActive == false) {
         responsiveMenu.classList.add('displayed-responsive-menu');
         responsiveBar.classList.add('cross');
         responsiveNav.classList.add('displayed-nav-menu');
         for (anchor of menuAnchors) {
             anchor.classList.add('displayed-nav-anchor');
         }
-        counter++;
-    } else if (counter == 1) {
+        isMenuActive = true;
+    } else if (isMenuActive == true) {
         responsiveMenu.classList.remove('displayed-responsive-menu');
         responsiveBar.classList.remove('cross');
         responsiveNav.classList.remove('displayed-nav-menu');
         for (anchor of menuAnchors) {
             anchor.classList.remove('displayed-nav-anchor');
         }
-        counter--;
+        isMenuActive = false;
     }
+    console.log(isMenuActive);
 }
 
 function withdrawMenu () {
     deployMenu();
+}
+
+let isGalleryActive = false;
+
+function deploySuccessStories () {
+    if (isGalleryActive == false) {
+        successStoriesGallery.classList.add('deploy-gallery');
+        isGalleryActive = true;
+    } else if (isGalleryActive == true) {
+        successStoriesGallery.classList.remove('deploy-gallery');
+        isGalleryActive = false;
+        window.scroll({
+            top: 7870,
+            left: 0,
+            behavior: "smooth",
+          });
+    }
 }
